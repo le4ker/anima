778619769170d9ca2b0744b572984cba619dd3ba
Panos Sakkos <panos.sakkos@gmail.com>
2013-11-13 18:47:47 +0200

    Added simulation class

---
+++ WriteRightSimulator/WriteRightSimulator.cs
@@ -0,0 +1,14 @@
+using System;
+
+using Di.Kdd.PredictionEngine;
+
+namespace Di.Kdd.WriteRightSimulator
+{
+	public class WriteRightSimulator : PredictionEngine
+	{
+		public WriteRightSimulator ()
+		{
+		}
+	}
+}
+
