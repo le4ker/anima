feccd65e0a7f80543c8fe1af90dc01cf3de57139
Panos Sakkos <panos.sakkos@gmail.com>
2013-11-13 18:43:26 +0200

    Added project for writeright simulation

---
+++ WriteRightSimulator/Main.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace WriteRightSimulator
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World of simulations!");
+		}
+	}
+}
